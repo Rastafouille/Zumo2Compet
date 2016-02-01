d3f91603b7733b1d4d23886b3601cfa88c2f3c13
rastafouille <jeremy.seyssaud@cea.fr>
2016-02-01 14:50:31 +0100

    mise a jour arduino code

--- Zumo2CompetArduino/Zumo2CompetArduino.ino
+++ Zumo2CompetArduino/Zumo2CompetArduino.ino
@@ -1,8 +1,9 @@
 
 
 //A FAIRE
-//- interruption sur les capteurs de reflectance
-//- sil ne detecte rien sur un tour, avancer un peu
+//- interruption sur les capteurs de reflectance      ??
+//- sil ne detecte rien sur un tour, avancer un peu   
+//-regler les seuil de reflectance en dur             OK
 
 
 #include <Wire.h>
@@ -14,25 +15,24 @@
 #define LED 13
 float A=6000;
 float B=76;
-int seuil=0;
 int BatteryPin = 1;
 float batterylevel = 0.0;
 float BatterySeuil = 3.0;
 
 // these might need to be tuned for different motor types
 #define REVERSE_SPEED     150 // 0 is stopped, 400 is full speed
-#define TURN_SPEED        150
+#define TURN_SPEED        200
 #define FORWARD_SPEED     150
-#define FORWARD_SPEED2    300
+#define FORWARD_SPEED2    400
 #define REVERSE_DURATION  400 // ms
 #define TURN_DURATION     400 // ms
 
-int AvantDistPin = 3;     
+int AvantDistPin = 0;     
 //int ArriereDistPin =1; battery level dessus !
-int GaucheDistPin = 0;
+int GaucheDistPin = 3;
 int DroiteDistPin = 2;
 
-int distAdv = 40;
+int distAdv = 30;
 
 int val = 0;           
 
@@ -57,14 +57,25 @@ void setup() {
  Serial.print( "## BATTERY LEVEL = ");Serial.print(batterylevel);Serial.println (" VOLTS");
  Serial.println("## INITIALISATION reflectance sensors");reflectanceSensors.init(pins, 3);
  buzzer.playNote(NOTE_G(4), 500, 9);  
- Serial.println("## INITIALISATION calibration");
- calibration ();
- seuil=350;
- buzzer.playNote(NOTE_G(4), 500, 9);  
+ //Serial.println("## INITIALISATION calibration");
+ //calibration ();
+ //seuil=350;
+ //buzzer.playNote(NOTE_G(4), 500, 9);  
  Serial.println("## Wait For Button to start");waitForButtonAndCountDown();
  motors.setSpeeds(FORWARD_SPEED2, FORWARD_SPEED2);
  delay(300);
 }
+
+//ROUGE 1076 720 1128
+//NOIR 2000 2000 2000
+//BLANC 980 716 1056
+
+
+//NOIR=430-450
+//BLANC=130-160
+//MARRON=250-300
+int seuil=230;
+
 void loop()
 {
   BatteryLevel();
@@ -75,24 +86,26 @@ void loop()
     button.waitForRelease();
     Serial.println("## Wait For Button to restart");waitForButtonAndCountDown();
   }
-  reflectanceSensors.readLine(sensorValues);
+  reflectanceSensors.read(sensorValues);
+  //Serial.print("reflectance :"); Serial.println(sensorValues[0]);
  //Envoie les infos de la centrale
   envoie();
   detectbordure(seuil);
   //Serial.println(dist(AvantDistPin));
   if (dist(AvantDistPin)<=distAdv)
     //AVANCE
-    {motors.setSpeeds(FORWARD_SPEED2, FORWARD_SPEED2);}
+    {Serial.println("## Adversaire detecte !!");
+    motors.setSpeeds(FORWARD_SPEED2, FORWARD_SPEED2);}
   else {detectadv();}
 }
 
-float dist(int pin)
+float dist(int pin_dist)
 {
    int n=0;
    float mean=0;
    long sensorsum=0;
-   while (n<50)
-    {n++;int sensorvalue=analogRead(pin);
+   while (n<20)
+    {n++;int sensorvalue=analogRead(pin_dist);
     sensorsum=sensorsum+sensorvalue;
     }
    mean=A/(sensorsum/n-B);
@@ -101,7 +114,9 @@ float dist(int pin)
 
 
 void BatteryLevel()
-{ batterylevel = float(analogRead(BatteryPin))/1024*5;}  
+{   
+  batterylevel = float(analogRead(BatteryPin))/1024*5;
+}  
   
 
 void calibration()
@@ -133,11 +148,10 @@ void waitForButtonAndCountDown()
   Serial.println("## Button pressed");
   digitalWrite(LED, LOW);
   // play audible countdown
-  for (int i = 0; i < 3; i++)
+  for (int i = 0; i < 4; i++)
   {Serial.println(5-i);delay(1000);buzzer.playNote(NOTE_G(3), 200, 9);}
-  Serial.println("2");delay(1000);Serial.println("1");
+  Serial.println("1");delay(1000);Serial.println("## GOGOGO !!!");
   buzzer.playNote(NOTE_G(4), 500, 9);  
-  delay(1000);Serial.println("## GOGOGO !!!");
 }
 
 void envoie()
@@ -150,15 +164,15 @@ timer = millis();
   Serial.print (", vr ");Serial.print (vright);
   Serial.print (", av ");Serial.print (dist(AvantDistPin));
   //Serial.print (", ar ");Serial.print (dist(ArriereDistPin));
-  Serial.print (", le ");Serial.print (dist(GaucheDistPin));
-  Serial.print (", ri ");Serial.print (dist(DroiteDistPin));
+  //Serial.print (", le ");Serial.print (dist(GaucheDistPin));
+  //Serial.print (", ri ");Serial.print (dist(DroiteDistPin));
   Serial.print (", Solleft ");Serial.print (sensorValues[0]);
   Serial.print (", Solright ");Serial.print (sensorValues[2]);
   Serial.println();  
 }
 void detectbordure(int seuil)
 {
-if (sensorValues[0] > seuil)
+if (sensorValues[0] < seuil)
   {
     // if leftmost sensor detects line, reverse and turn to the right
     motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
@@ -166,8 +180,9 @@ if (sensorValues[0] > seuil)
     motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
     delay(TURN_DURATION);
     motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+    Serial.println("## Bordure detectee !!");
   }
-  else if (sensorValues[2] > seuil)
+  else if (sensorValues[2] < seuil)
   {
     // if rightmost sensor detects line, reverse and turn to the left
     motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
@@ -175,18 +190,26 @@ if (sensorValues[0] > seuil)
     motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
     delay(TURN_DURATION);
     motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+    Serial.println("## Bordure detectee !!");
   }
   else
   {
     // otherwise, go straight
-    motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+    //motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
   }
 }
 
 void detectadv ()
-{
- while (dist(AvantDistPin)>=distAdv)
+{ Serial.println( "## DETECTION ADVERSAIRE en cours...");
+ long time=millis();
+ while (dist(AvantDistPin)>=distAdv && (millis()-time)<2000)
   //ROTATION
-    {motors.setSpeeds(TURN_SPEED, -TURN_SPEED);delay(50);}
+    {
+      Serial.println((millis()-time));
+    motors.setSpeeds(TURN_SPEED, -TURN_SPEED);delay(5);
+    }
+
+    motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);delay(300);
+
 }
 
